a47bc911d12499030f8c6837e6be42ba4de1eb13
Shen <72716300+shensandoval@users.noreply.github.com>
2021-05-13 20:26:52 +0800

    Update Table.vue

--- frontend/src/views/Table.vue
+++ frontend/src/views/Table.vue
@@ -31,3 +31,43 @@ export default {
   }
 };
 </script>
+
+
+addRow() {
+    this.rows.push({
+      row: []
+    });
+  },
+  removeRow: function(index) {
+    console.log("Removing", index);
+    this.rows.splice(index, 1);
+  }
+  
+  
+  <template>
+  <b-container class="mt-4">
+    <b-table :items="items" :current-page="currentPage" :per-page="perPage" striped></b-table>
+
+    <b-pagination v-model="currentPage" :total-rows="items.length" :per-page="perPage"></b-pagination>
+  </b-container>
+</template>
+
+<script>
+const itemCount = 15000;
+const items = Array.from({ length: itemCount }, (v, i) => ({
+  isActive: i % 2 === 0,
+  age: i,
+  first_name: `Dickerson ${i}`,
+  last_name: `Macdonald ${i}`
+}));
+
+export default {
+  data() {
+    return {
+      items,
+      currentPage: 0,
+      perPage: 10
+    };
+  }
+};
+</script>
